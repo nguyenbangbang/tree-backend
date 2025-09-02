91a61e35b66f1a8abb8e08143065e009a0f33557
BangNguyen <bangbangnguyen675@gmail.com>
2025-09-03 01:36:46 +0700

    3/9/2025

--- index.js
+++ index.js
@@ -7,16 +7,10 @@ require('dotenv').config()
 
 // middleware
 app.use(express.json());
-const corsOptions = {
-  origin: ['http://localhost:5173', 'https://tree-frontend-lake.vercel.app'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-};
-
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions)); // xử lý preflight
-
+app.use(cors({
+    origin: ['http://localhost:5173',"https://tree-frontend-lake.vercel.app"],
+    credentials: true
+}))
 
 //route
 const treeRoutes = require('./src/trees/tree.route');
